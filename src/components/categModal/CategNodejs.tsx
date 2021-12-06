451fcfe8a9a178e3679a9e4793b42d0bc607553f
martin162000 <martin162000@gmail.com>
2021-12-06 09:43:50 +0100

    Zivotopis aktualizacia

--- src/components/categModal/CategNodejs.tsx
+++ src/components/categModal/CategNodejs.tsx
@@ -24,7 +24,10 @@ const CategNodejs = (props: Props) => {
             <h4>Skúsenosti</h4>
             <article>
                 <p>S týmto jazykom som začal pracovať v roku 2016. Vytvoril som pár scriptov na stránky, ktoré som mal. Jednou z nich bola stránka, kde si uživatelia mohli vymienať položky medzi sebou s použitím steamApi.
-                   Použil som nodejs aj na maturitnú prácu, ako "scrap bota", ktorý bral produkty z rôznych stránok ako je footshop.sk, thestreets.sk atď. a ukladal ich do MySql databázy. Chcem tento jazyk aplikovať, ako backend pri
+                   Použil som nodejs aj na maturitnú prácu, ako "scrap bota", ktorý bral produkty z rôznych stránok ako je footshop.sk, thestreets.sk atď. a ukladal ich do MySql databázy. 
+                   Nodejs som využil aj pri aplikácií "Movie app", ktorú som robil v reacte. Vytvoril som registráciu uživateľov. Uživatelia môžu editovať svoj profil, pridávať a mazať filmy. Všetky tieto dáta sú uložené v MySQL databáze.
+                   Táto aplikácia "Movie app" je voľne dostupná na mojom <a href="https://github.com/martin162000" target="_blank" rel="noopener noreferrer"> githube</a>.
+                   Chcem tento jazyk aplikovať, ako backend pri
                    react aplikáciach.
                 </p>
                     
--- src/components/categModal/CategReact.tsx
+++ src/components/categModal/CategReact.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import movieApp from '../../assets/img/reactApps/movieApp.png';
+import movieAppBackend from '../../assets/img/reactApps/movieAppWithBackend.png';
+import utulky from '../../assets/img/reactApps/utulkyApp.png';
 import reminderApp from '../../assets/img/reactApps/reminderApp.png';
 import zivotopisApp from '../../assets/img/reactApps/zivotopisApp.png';
 
@@ -32,9 +34,10 @@ const CategReact = (props: Props) => {
                   iba som sa na nej učil pracovať s reactom. Neskôr som sa rozhodol vytvoriť viac webových aplikácií, ktoré sú voľne dostupné na mojom
                   githube.</p>
             </article>
+            
   
                <div className="apps">
-                  <h5>1.Movie app</h5>
+                  <h5>1.Movie app </h5>
                   <p> Aplikácia, ktorá získava dáta z api http://www.omdbapi.com/. 
                     Zobrazí filmy, ktoré si následne môžte rozklinuť pre detailnejšie info. 
                     Keď kliknete na hviezdiu vľavo hore v detailoch, tak vám film uloží do podstránky
@@ -49,9 +52,42 @@ const CategReact = (props: Props) => {
                       </div>
                  </div>
                </div>
+
+               <div className="apps">
+                  <h5>2.Movie app + Nodejs + MySQL</h5>
+                  <p> Je to tá istá aplikácia ako predchádzajúca (Movie app), lenže
+                    pridal som k nej backend, ktorý som programoval v Nodejs a databázu som urobil v MySQL.
+                    Teraz sa do aplikácie môže zaregistrovať uživateľ, potom sa príhlásiť, editovať svoj profil (zmena emailu / hesla), môže pridávať filmy na svoj účet (aj ich zmazať)
+                    a nakoniec sa môže uživateľ aj odhlásiť. Keď sa uživateľ registruje, tak heslo sa hashuje cez bcrypt a aj saltuje kvôli bezpečnosti. Expirácia cookie je nastavená na 3 dni. 
+                    Uživateľ musí byť unikátny a to je nastavené v MySQL databáze. </p>
+                  <p className="usesLib">Použil som React-redux, Route, Axios, Nodejs (express), MySQL</p>
+                  <div className="gridButtons">
+                    <img src={movieAppBackend} alt="Movie app img" />
+                      <div>
+                          <div className="btnBlock"><a href="https://github.com/martin162000/MovieApp_react-nodejs-mysql" className="btn btnSec" target="_blank" rel="noopener noreferrer"> Zobraziť zdrojový kód</a></div>
+                      </div>
+                 </div>
+               </div>
+
+               <div className="apps">
+                  <h5>3.Utulky (zadanie GoodRequest.com)</h5>
+                  <p> Dostal som zadanie z firmy Goodrequest.com, ktoré som vypracoval za 5 dni. 
+                     Celé zadanie je na mojom githube v "README.md", ktoré nájdete, keď kliknete na "Zobraziť zdrojový kód".
+                     Návrh frontendu som dostal figma.com.
+                     Musel som overiť formuláre, data manažment spracovať cez react-redux, pomocou GET získať dáta z API a nakoniec poslať údaje pomocou POST a zobraziť výpis, či formulár bol správne vyplnený.
+                     </p>
+                  <p className="usesLib">Použil som lokalizačnú knižnicu na stringy i18next, React-redux, implementoval som aj SEO</p>
+                  <div className="gridButtons">
+                    <img src={utulky} alt="Utulky app img" />
+                      <div>
+                          <div className="btnBlock"><a href="https://martin162000.github.io/utulky/" className="btn" target="_blank" rel="noopener noreferrer"> Zobraziť aplikáciu</a></div>
+                          <div className="btnBlock"><a href="https://github.com/martin162000/utulky" className="btn btnSec" target="_blank" rel="noopener noreferrer"> Zobraziť zdrojový kód</a></div>
+                      </div>
+                 </div>
+            </div>
   
                <div className="apps">
-                  <h5>2.Reminder app</h5>
+                  <h5>4.Reminder app</h5>
                   <p> Použivateľ si môže vytvoriť príspevok, tento príspevok neskôr môže upravovať alebo
                      ho môže aj vymazať. Pri tejto aplikácii som sa snažil nepoužiť React-redux, pretože som chcel 
                      presnejšie pochopiť, ako prebieha menenie stavu
@@ -68,7 +104,7 @@ const CategReact = (props: Props) => {
             </div>
   
             <div className="apps">
-                  <h5>3.Životopis</h5>
+                  <h5>5.Životopis</h5>
                   <p> Vytvoril som svoj vlastný životopis v reacte.</p>
                   <p className="usesLib">Použil som Context, Bootstrap icons</p>
                   <div className="gridButtons">
